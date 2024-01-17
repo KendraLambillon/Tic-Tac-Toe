a4fd6ff569fa3ae0be07b3440316310148f2f1a3
KendraLa <kendralambillon@gmail.com>
2024-01-17 12:47:24 +0100

    detect winner

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import { useState } from 'react'
 import { Board } from './components/Board'
 import { Turns } from './constants'
 import { Square } from './components/Square'
+import { checkWinner } from './logic/board'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(Turns.X) // initial value is X
+  const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
-    if (board[index]) return
+    if (board[index] || winner) return
+
     const newTurn = turn === Turns.X ? Turns.O : Turns.X
     setTurn(newTurn)
 
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
 
---
+++ src/logic/board.js
@@ -0,0 +1,14 @@
+import { Winner_combos } from "../constants";
+
+export const checkWinner = (boardToCheck) => {
+    for (const combo of Winner_combos) {
+        const [a, b, c] = combo
+        if (
+            boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+            return boardToCheck[a]
+        }
+    }
+}
